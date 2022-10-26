b372377b2625a57fea9371cb6d036a8c69d810c3
Phan Văn Học <phanhocit@gmail.com>
2022-10-26 14:08:30 +0700

    Implement periodic vesting account

---
+++ x/auth/vesting/msg_server.go
@@ -0,0 +1,85 @@
+package vesting
+
+import (
+	"context"
+	"github.com/armon/go-metrics"
+	"github.com/aura-nw/aura/x/auth/vesting/types"
+	"github.com/cosmos/cosmos-sdk/telemetry"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	org_types "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
+)
+
+type msgServer struct {
+	keeper.AccountKeeper
+	types.BankKeeper
+}
+
+// NewMsgServerImpl returns an implementation of the vesting MsgServer interface,
+// wrapping the corresponding AccountKeeper and BankKeeper.
+func NewMsgServerImpl(k keeper.AccountKeeper, bk types.BankKeeper) types.MsgExtendServer {
+	return &msgServer{AccountKeeper: k, BankKeeper: bk}
+}
+
+var _ types.MsgExtendServer = msgServer{}
+
+func (s msgServer) CreatePeriodicVestingAccount(goCtx context.Context, msg *types.MsgCreatePeriodicVestingAccount) (*types.MsgCreatePeriodicVestingAccountResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	ak := s.AccountKeeper
+	bk := s.BankKeeper
+
+	from, err := sdk.AccAddressFromBech32(msg.FromAddress)
+	if err != nil {
+		return nil, err
+	}
+	to, err := sdk.AccAddressFromBech32(msg.ToAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	if acc := ak.GetAccount(ctx, to); acc != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "account %s already exists", msg.ToAddress)
+	}
+
+	var totalCoins sdk.Coins
+
+	for _, period := range msg.VestingPeriods {
+		totalCoins = totalCoins.Add(period.Amount...)
+	}
+
+	baseAccount := authtypes.NewBaseAccountWithAddress(to)
+	baseAccount = ak.NewAccount(ctx, baseAccount).(*authtypes.BaseAccount)
+	vestingAccount := org_types.NewPeriodicVestingAccount(baseAccount, totalCoins.Sort(), msg.StartTime, msg.VestingPeriods)
+
+	ak.SetAccount(ctx, vestingAccount)
+
+	defer func() {
+		telemetry.IncrCounter(1, "new", "account")
+
+		for _, a := range totalCoins {
+			if a.Amount.IsInt64() {
+				telemetry.SetGaugeWithLabels(
+					[]string{"tx", "msg", "create_periodic_vesting_account"},
+					float32(a.Amount.Int64()),
+					[]metrics.Label{telemetry.NewLabel("denom", a.Denom)},
+				)
+			}
+		}
+	}()
+
+	err = bk.SendCoins(ctx, from, to, totalCoins)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, org_types.AttributeValueCategory),
+		),
+	)
+	return &types.MsgCreatePeriodicVestingAccountResponse{}, nil
+}

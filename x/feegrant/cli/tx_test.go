fc1d9a40f9f0f6893c81c52c6d134c273cf391e4
tiennv1997 <tien.nv291997@gmail.com>
2023-02-10 13:38:48 +0700

    Fix test feegrant

--- x/feegrant/cli/tx_test.go
+++ x/feegrant/cli/tx_test.go
@@ -2,55 +2,11 @@ package cli_test
 
 import (
 	"github.com/aura-nw/aura/x/feegrant/cli"
-	"github.com/spf13/cobra"
-	"reflect"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestGetTxCmd(t *testing.T) {
-	tests := []struct {
-		name string
-		want *cobra.Command
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := cli.GetTxCmd(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetTxCmd() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestNewCmdFeeGrant(t *testing.T) {
-	tests := []struct {
-		name string
-		want *cobra.Command
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := cli.NewCmdFeeGrant(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewCmdFeeGrant() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestNewCmdRevokeFeegrant(t *testing.T) {
-	tests := []struct {
-		name string
-		want *cobra.Command
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := cli.NewCmdRevokeFeegrant(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewCmdRevokeFeegrant() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	txCmd := cli.GetTxCmd()
+	require.Equal(t, len(txCmd.Commands()), 2)
 }

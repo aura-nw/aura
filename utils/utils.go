abb73480a374465a622548dfa0faf13818c77a6e
phamphong9981 <phamphong9981@gmail.com>
2023-02-16 14:34:48 +0700

    utils

---
+++ utils/utils.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"context"
+	"strconv"
+
+	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+// RemoveDuplicateValues removes the duplicated values from the given slice
+func RemoveDuplicateValues(slice []string) []string {
+	keys := make(map[string]bool)
+	var list []string
+
+	for _, entry := range slice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+// GetHeightRequestContext adds the height to the context for queries
+func GetHeightRequestContext(context context.Context, height int64) context.Context {
+	return metadata.AppendToOutgoingContext(
+		context,
+		grpctypes.GRPCBlockHeightHeader,
+		strconv.FormatInt(height, 10),
+	)
+}

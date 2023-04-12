c2b5e9dddf3fbb1b569294dd6eee72bec1ae5f49
phanhoc <phanhocit@gmail.com>
2023-04-12 16:25:57 +0700

    Upgrade wasmd to 0.31 and refactor code

--- x/ibc-middleware/middleware.go
+++ x/ibc-middleware/middleware.go
@@ -14,14 +14,14 @@ import (
 var _ porttypes.Middleware = &IBCMiddleware{}
 
 type IBCMiddleware struct {
-	App             porttypes.IBCModule
-	ics4_middleware *ICS4Middleware
+	App            porttypes.IBCModule
+	Ics4Middleware *ICS4Middleware
 }
 
-func NewIBCMiddleware(app porttypes.IBCModule, ics4_middleware *ICS4Middleware) IBCMiddleware {
+func NewIBCMiddleware(app porttypes.IBCModule, ics4Middleware *ICS4Middleware) IBCMiddleware {
 	return IBCMiddleware{
-		App:             app,
-		ics4_middleware: ics4_middleware,
+		App:            app,
+		Ics4Middleware: ics4Middleware,
 	}
 }
 
@@ -117,7 +117,7 @@ func (im IBCMiddleware) OnRecvPacket(
 ) ibcexported.Acknowledgement {
 
 	// Override OnRecvPacket method for parsing memo and executing wasm
-	return im.ics4_middleware.wasm.OnRecvPacketOverride(im, ctx, packet, relayer)
+	return im.Ics4Middleware.wasm.OnRecvPacketOverride(im, ctx, packet, relayer)
 }
 
 // OnAcknowledgementPacket implements the IBCMiddleware interface
@@ -129,7 +129,7 @@ func (im IBCMiddleware) OnAcknowledgementPacket(
 ) error {
 
 	// Override OnAcknowledgementPacket method for executing contract callback in ack receiving case
-	return im.ics4_middleware.wasm.OnAcknowledgementPacketOverride(im, ctx, packet, acknowledgement, relayer)
+	return im.Ics4Middleware.wasm.OnAcknowledgementPacketOverride(im, ctx, packet, acknowledgement, relayer)
 }
 
 // OnTimeoutPacket implements the IBCMiddleware interface
@@ -140,7 +140,7 @@ func (im IBCMiddleware) OnTimeoutPacket(
 ) error {
 
 	// Override OnTimeourPacket method for executing contract callback in timeout case
-	return im.ics4_middleware.wasm.OnTimeoutPacketOverride(im, ctx, packet, relayer)
+	return im.Ics4Middleware.wasm.OnTimeoutPacketOverride(im, ctx, packet, relayer)
 }
 
 // SendPacket implements the ICS4 Wrapper interface
@@ -150,7 +150,7 @@ func (im IBCMiddleware) SendPacket(
 	packet ibcexported.PacketI,
 ) error {
 
-	return im.ics4_middleware.SendPacket(ctx, chanCap, packet)
+	return im.Ics4Middleware.SendPacket(ctx, chanCap, packet)
 }
 
 // WriteAcknowledgement implements the ICS4 Wrapper interface
@@ -161,7 +161,7 @@ func (im IBCMiddleware) WriteAcknowledgement(
 	ack ibcexported.Acknowledgement,
 ) error {
 
-	err := im.ics4_middleware.WriteAcknowledgement(ctx, chanCap, packet, ack)
+	err := im.Ics4Middleware.WriteAcknowledgement(ctx, chanCap, packet, ack)
 
 	return err
 }
@@ -169,5 +169,5 @@ func (im IBCMiddleware) WriteAcknowledgement(
 // GetAppVersion implements the ICS4 Wrapper interface
 func (im IBCMiddleware) GetAppVersion(ctx sdk.Context, portID, channelID string) (string, bool) {
 
-	return im.ics4_middleware.GetAppVersion(ctx, portID, channelID)
+	return im.Ics4Middleware.GetAppVersion(ctx, portID, channelID)
 }

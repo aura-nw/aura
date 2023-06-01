409204160d57693825786ef2a4635b2191d8d4b5
Narutobacoshiba <haphapbk29@gmail.com>
2023-06-01 11:39:33 +0700

    Add smart_account_id:uint64 to genesis

--- x/smartaccount/genesis.go
+++ x/smartaccount/genesis.go
@@ -1,24 +1,26 @@
 package smartaccount
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/aura-nw/aura/x/smartaccount/keeper"
 	"github.com/aura-nw/aura/x/smartaccount/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // InitGenesis initializes the capability module's state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
-    // this line is used by starport scaffolding # genesis/module/init
+	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
+
+	k.SetNextAccountID(ctx, genState.GetSmartAccountId())
 }
 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
+	genesis.SmartAccountId = k.GetNextAccountID(ctx)
+	// this line is used by starport scaffolding # genesis/module/export
 
-    // this line is used by starport scaffolding # genesis/module/export
-
-    return genesis
+	return genesis
 }

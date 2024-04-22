4a8c3e5d6006bcca43bb43fd2537b8f4d64a087b
kienvc <vuchikien269@gmail.com>
2024-04-22 14:50:39 +0700

    update evm denom

--- x/evmutil/keeper/bank_keeper.go
+++ x/evmutil/keeper/bank_keeper.go
@@ -14,10 +14,10 @@ import (
 
 const (
 	// EvmDenom is the gas denom used by the evm
-	EvmDenom = "aaura"
+	EvmDenom = "aeaura"
 
 	// CosmosDenom is the gas denom used by the kava app
-	CosmosDenom = "uaura"
+	CosmosDenom = "ueaura"
 )
 
 // ConversionMultiplier is the conversion multiplier between akava and ukava

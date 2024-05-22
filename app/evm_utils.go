5a1e13baec3521e2122204e8b47a3a388f34c418
Vu Ngoc Quang <quang.vn@outlook.com>
2024-05-22 11:10:37 +0700

    remove bank precompile

--- app/evm_utils.go
+++ app/evm_utils.go
@@ -18,7 +18,6 @@ import (
 	distributionkeeper "github.com/cosmos/cosmos-sdk/x/distribution/keeper"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	channelkeeper "github.com/cosmos/ibc-go/v7/modules/core/04-channel/keeper"
-	bankprecompile "github.com/evmos/evmos/v16/precompiles/bank"
 	ics20precompile "github.com/evmos/evmos/v16/precompiles/ics20"
 	"github.com/evmos/evmos/v16/precompiles/p256"
 	erc20Keeper "github.com/evmos/evmos/v16/x/erc20/keeper"
@@ -61,11 +60,6 @@ func Precompiles(
 		panic(fmt.Errorf("failed to instantiate ICS20 precompile: %w", err))
 	}
 
-	bankPrecompile, err := bankprecompile.NewPrecompile(bankKeeper, erc20Keeper)
-	if err != nil {
-		panic(fmt.Errorf("failed to instantiate bank precompile: %w", err))
-	}
-
 	// Stateless precompiles
 	precompiles[bech32Precompile.Address()] = bech32Precompile
 	precompiles[p256Precompile.Address()] = p256Precompile
@@ -74,7 +68,6 @@ func Precompiles(
 	precompiles[stakingPrecompile.Address()] = stakingPrecompile
 	precompiles[distributionPrecompile.Address()] = distributionPrecompile
 	precompiles[ibcTransferPrecompile.Address()] = ibcTransferPrecompile
-	precompiles[bankPrecompile.Address()] = bankPrecompile
 
 	return precompiles
 }

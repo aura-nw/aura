ae45a4ef71da0e09543a926825662c555af507bd
kienvc <17985897+kienvc@users.noreply.github.com>
2024-08-13 15:13:22 +0700

    change mainnet denom

--- x/evmutil/keeper/bank_keeper.go
+++ x/evmutil/keeper/bank_keeper.go
@@ -14,10 +14,10 @@ import (
 
 const (
 	// EvmDenom is the gas denom used by the evm
-	EvmDenom = "aeaura"
+	EvmDenom = "aaura"
 
 	// CosmosDenom is the gas denom used by the kava app
-	CosmosDenom = "ueaura"
+	CosmosDenom = "uaura"
 )
 
 // ConversionMultiplier is the conversion multiplier between akava and ukava
